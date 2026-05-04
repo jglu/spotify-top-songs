ef7df6fe74f1b20099726f30e58d08c1e0ade546
Jessica Lu <jgujialu@gmail.com>
2026-05-03 21:40:38 -0400

    feat: sync playlist instead of replacing every day, to track how long a song has been in the top 50
    - literally why did i make this a flask app is it because i wanted a ui.??
    
    TODO:
    - install Black and format
    - refactor (into services, split up the token refresh)
    - migrate to fastapi

--- auto_update.py
+++ auto_update.py
@@ -1,14 +1,9 @@
 import os
 import requests
-from datetime import datetime
 from dotenv import load_dotenv
 
 from api import (
-    get_existing_URIs,
-    delete_existing_songs,
-    get_new_URIs,
-    add_new_songs,
-    update_playlist_description
+    run_playlist_update
 )
 
 load_dotenv()
@@ -35,13 +30,7 @@ def refresh_access_token():
 
 def auto_update_playlist():
     token = refresh_access_token()
-
-    existing_URIs = get_existing_URIs(PLAYLIST_ID, token)
-    delete_existing_songs(PLAYLIST_ID, existing_URIs, token)
-    new_URIs = get_new_URIs(token)
-    add_new_songs(PLAYLIST_ID, new_URIs, token)
-    update_playlist_description(PLAYLIST_ID, token)
-
+    run_playlist_update(PLAYLIST_ID, token)
     print("Playlist updated!")
 
 if __name__ == "__main__":
